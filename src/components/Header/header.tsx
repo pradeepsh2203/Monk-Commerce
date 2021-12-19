3a8cb5b508b7bd422449da736f0d662b6b749b34
Pradeep Sharma <pradeep.sharma.cd.mec19@itbhu.ac.in>
2021-12-19 17:42:10 +0530

    Made Header Responsive

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,21 +1,49 @@
-import React, {useState} from 'react';
-import Logo from '../../data/img/logo.svg'
-import './header.css'
+import React, { useState, useEffect } from "react";
+import Logo from "../../data/img/logo.svg";
+import "./header.css";
 
-export const Header: React.FC =()=>{
-    const [IsNavVisible, setNavVisible] = useState(true);
-    return (
-        <div className='header_container'>
-            <img src={Logo} alt="Logo" className="header_logo" />
-            { IsNavVisible && <div className="header_container header_links">
-                <p>Pricing</p>
-                <p>Testimonial</p>
-                <span style={{width:"60px"}}></span>
-                <p className='bold'>Sign In</p>
-                <p className='focus'>Sign Up</p>
-            </div>}
+export const Header: React.FC = () => {
+	const [IsNavVisible, setNavVisible] = useState(false);
+	let isScreenSizeSmall: boolean = false;
 
-            <button className='header_button' onClick={()=>setNavVisible(true)}>CLick me</button>
-        </div>
-    )
-}
+	let [screenWidth, setScreenWidth] = useState(window.innerWidth);
+	useEffect(() => {
+		const changeWindowSize = () => setScreenWidth(window.innerWidth);
+		window.addEventListener("resize", changeWindowSize);
+		return () => window.removeEventListener("resize", changeWindowSize);
+	}, []);
+
+	if (screenWidth < 1000) isScreenSizeSmall = true;
+
+	return (
+		<div className="header_container">
+			<img src={Logo} alt="Logo" className="header_logo" />
+			{(!isScreenSizeSmall || IsNavVisible) && (
+				<div className="header_links">
+					<p>Pricing</p>
+					<p>Testimonial</p>
+					<span style={{ width: "60px" }}></span>
+					<p className="bold">Sign In</p>
+					<p className="focus">Sign Up</p>
+				</div>
+			)}
+
+			<button
+				className="header_button"
+				onClick={() => setNavVisible(!IsNavVisible)}
+			>
+				{IsNavVisible ? (
+					<i
+						className="fas fa-times fa-2x"
+						style={{ color: "#aaa" }}
+					></i>
+				) : (
+					<i
+						className="fas fa-align-right fa-2x"
+						style={{ color: "#F53838" }}
+					></i>
+				)}
+			</button>
+		</div>
+	);
+};
